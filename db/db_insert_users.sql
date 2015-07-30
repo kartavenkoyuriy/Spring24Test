13a984525087ada7968dca341831e287ddf08ea5
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2015-07-30 12:00:04 +0300

    Third commit

---
+++ db/db_insert_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO `my_db_test`.`db_users` (`login`, `password`, `isAdmin`) VALUES ('admin', 'admin', '1');
+INSERT INTO `my_db_test`.`db_users` (`login`, `password`, `isAdmin`) VALUES ('user1', 'user1', '0');
+INSERT INTO `my_db_test`.`db_users` (`login`, `password`, `isAdmin`) VALUES ('user2', 'user2', '0');
+INSERT INTO `my_db_test`.`db_users` (`login`, `password`, `isAdmin`) VALUES ('vasya', 'vasya', '0');
